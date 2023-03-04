20a16523744754624b9feb9ad67c5353b6656f60
Nidhi Satija <126676344+nidhi-codeyoung@users.noreply.github.com>
2023-03-04 21:54:22 +0530

    Add files via upload

---
+++ Exercise 38.2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Flipcoin from './Flipcoin'
+
+const App=()=> {
+return (
+	<div className="App">
+    <center>
+	<Flipcoin />
+  </center>
+	</div>
+);
+}
+
+export default App;
